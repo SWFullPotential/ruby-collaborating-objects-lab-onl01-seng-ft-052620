59d4a2917429f1be7fb5b19bacf2aa7f337642e8
Samantha Wabi <swfullpotential@gmail.com>
2020-06-10 02:46:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,10 +18,11 @@ class Song
     @artist.name
   end
   
-    def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
+  def self.new_by_filename(file)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
     song
   end
 end
