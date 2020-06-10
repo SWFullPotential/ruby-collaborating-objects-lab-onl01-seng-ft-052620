6b4b189d17f07db3e2d129dd5271b83ec05b3717
Samantha Wabi <swfullpotential@gmail.com>
2020-06-10 01:46:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,20 @@
 class Artist 
-  attr_accessor :name 
+  attr_accessor :name, :songs
     
     @@all = []
     
   def initialize(name)
     @name = name
+    @songs = song 
     @@all << self
   end
   
   def self.all 
     @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+    @songs << song
   end
 end
