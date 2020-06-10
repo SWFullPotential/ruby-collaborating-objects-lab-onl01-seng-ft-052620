8335171a4dd22e84b9bf227328a671277f3669c4
Samantha Wabi <swfullpotential@gmail.com>
2020-06-10 02:38:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,51 +1,28 @@
-# class Song 
+class Song 
   
-#   attr_accessor :name, :artist 
+  attr_accessor :name, :artist 
   
-#   @@all = []
+  @@all = []
   
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
-  
-#   def self.all
-#     @@all
-#   end
-  
-#   def artist_name
-#     return nil if @artist == nil
-#     @artist.name
-#   end
-  
-#   def self.new_by_filename(file)
-#     artist_name = file.split(" - ")[0]
-#     song_name = file.split(" - ")[1]
-#     song = Song.new(song_name)
-#     song.artist_name = artist_name
-#     song
-#   end
-# end
-
-
-class Song
-  attr_accessor :name, :artist
-
   def initialize(name)
     @name = name
+    @@all << self
   end
-
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    return nil if @artist == nil
+    @artist.name
+  end
+  
   def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
     song = Song.new(song_name)
     song.artist_name = artist_name
     song
-  end
-
-  def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
-    artist.add_song(self)
   end
 end
